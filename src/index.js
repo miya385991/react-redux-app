f34a527d86e35daf650e22ab644d709ecf7447a0
miya385991 <38mtyuusuuke@gmail.com>
2020-04-05 16:50:38 +0900

    implement event index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-import { createStore } from 'redux';
+import EventsIndex from './components/events_index';
+import { createStore,applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
-    <App />
+    <EventsIndex />
     </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
